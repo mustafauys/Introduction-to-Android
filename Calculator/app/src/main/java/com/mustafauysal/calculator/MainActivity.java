3561a58a9d75be3a7a27e5df37548bd12d7097a3
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2020-12-13 23:17:50 +0300

    BugFixed

--- Calculator/app/src/main/java/com/mustafauysal/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/mustafauysal/calculator/MainActivity.java
@@ -1,11 +1,4 @@
-package com.mustafauysal.calculator;
 
-import android.speech.tts.TextToSpeech;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
