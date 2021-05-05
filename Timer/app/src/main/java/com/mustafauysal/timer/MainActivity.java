36f3583d2de52752fd80dcef43d80cfda51c0232
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-05-05 23:54:45 +0300

    Counters

--- Timer/app/src/main/java/com/mustafauysal/timer/MainActivity.java
+++ Timer/app/src/main/java/com/mustafauysal/timer/MainActivity.java
@@ -8,35 +8,5 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        new CountDownTimer(10000, 1000) {
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-                TextView textView = (TextView) findViewById(R.id.textView);
-
-                textView.setText("Left: "+ millisUntilFinished / 1000);
-
-            }
-
-            @Override
-            public void onFinish() {
-
-                Toast.makeText(getApplicationContext(), "Time's Done", Toast.LENGTH_LONG).show();
-
-                TextView textView = (TextView) findViewById(R.id.textView);
-
-                textView.setText("Left : 0");
-
-            }
-        }.start();
-
-
-
-    }
+    
 }
